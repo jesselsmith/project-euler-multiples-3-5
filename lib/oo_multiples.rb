27a34ffe109e7f2c38834b858954942e7f755009
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-08 17:44:49 -0700

    commit tree 1409f266431abe393e77b03cf4b192a9d3b833b2

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,38 @@
 # Enter your object-oriented solution here!
+class Multiples
+    @multipleArray
+    def initialize(limit)
+        if limit.nil?
+            raise ArgumentError.new "There must be a limit!"
+        end
+        @limit = limit
+    end
+
+    def collect_multiples()
+        if(@multipleArray.nil?)
+            @multipleArray = []
+            (@limit - 1).times do |i|
+                if (i+1) % 3 == 0 || (i+1) % 5 == 0
+                    @multipleArray.push(i+1) 
+                end    
+            end
+        end
+        @multipleArray
+    end
+
+    def sum_multiples()
+        if !@multipleArray.nil?
+            @multipleArray.reduce do |sum, current|
+                sum + current
+            end  
+        else
+            sum = 0
+            (@limit - 1).times do |i|
+                if (i+1) % 3 == 0 || (i+1) % 5 == 0
+                    sum += i + 1
+                end    
+            end
+            sum
+        end
+    end
+end
