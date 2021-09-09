903377dfbb3df25528ee0d4cf80a048ce0153b0c
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-08 17:45:33 -0700

    solution added

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    multiples = []
+
+    (limit - 1).times do |i|
+        if (i+1) % 3 == 0 || (i+1) % 5 == 0
+            multiples.push(i+1) 
+        end    
+    end
+    multiples
+end
+
+def sum_multiples(limit)
+    sum = 0
+
+    (limit - 1).times do |i|
+        if (i+1) % 3 == 0 || (i+1) % 5 == 0
+            sum += i + 1
+        end    
+    end
+    sum
+end
